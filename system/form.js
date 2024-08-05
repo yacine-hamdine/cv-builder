992d646880e0ce38efc806c04ce5559939db1d0b
yacine-hamdine <yacinehamdine@gmail.com>
2024-08-05 17:33:08 +0100

    step 2 (builder) started

--- system/form.js
+++ system/form.js
@@ -1,51 +1,54 @@
-function convertToBase64(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload   
-   = () => resolve(reader.result);
-      reader.onerror = error => reject(error);   
-  
-    });
-  }
-
-let base64Photo = '';
-  
-  // Example usage:
-  const input = document.getElementById('photo');
-  input.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    convertToBase64(file)
-      .then(base64String => {
-        console.log(base64String);
-        // Do something with the base64 string
-        base64Photo = base64String;
-      })
-      .catch(error => {
-        console.error('Error converting image to base64:', error);
-      });
-  });
-
 function addField(group){
+    let n = Math.round(Math.random()*100000000);
     let rm = document.createElement('span');
     rm.innerHTML = '&#x274c;';
     rm.style.textAlign = 'right';
     rm.style.width = '90%';
     rm.style.cursor ='pointer';
-    let set = document.getElementById(group);
+    rm.classList.add(`added-${n}`);
     rm.onclick = () => {
-        set.querySelectorAll('.added').forEach(el => el.remove());
+        set.querySelectorAll(`.added-${n}`).forEach(el => el.remove());
     };
+    let set = document.getElementById(group);
     const template = document.querySelectorAll(`#${group} .template`);    
     const tmp = [];
     template.forEach(el => tmp.push(el.cloneNode(true)));
-    tmp.forEach(el => el.classList.remove('template'));
-    tmp.forEach(el => el.classList.add("added"));
-    tmp.forEach(el => el.setAttribute("name", `${el.name}-2`));
-    rm.classList.add("added");
-    set.insertBefore(rm, set.lastElementChild);
-    tmp.forEach(el => set.insertBefore(el, set.lastElementChild));
+    tmp.forEach(el => {
+      el.value = '';
+      el.classList.remove('template');
+      el.classList.add(`added-${n}`);
+      el.classList.add(`added-${n}`);
+      el.setAttribute("name", `${el.name}-2`);
+      set.insertBefore(el, set.lastElementChild);
+    });
+    set.insertBefore(rm, set.querySelector(`.added-${n}`));
 }
+
+function convertToBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload   = () => resolve(reader.result);
+    reader.onerror = error => reject(error);   
+
+  });
+}
+
+let base64Photo = '';
+
+// Example usage:
+const input = document.getElementById('photo');
+input.addEventListener('change', (event) => {
+  const file = event.target.files[0];
+  convertToBase64(file)
+    .then(base64String => {
+      // Do something with the base64 string
+      base64Photo = base64String;
+    })
+    .catch(error => {
+      console.error('Error converting image to base64:', error);
+    });
+});
 
 function convertFormDataToKeyValuePairs(form) {
     const formData = new FormData(form);
